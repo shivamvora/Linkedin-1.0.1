121e50bf9c320299c907a4f5eaaa89219324aa92
shivamvora <shivampatel27102001@gmail.com>
2021-05-10 20:52:05 +0530

    responsive design

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,13 +5,25 @@ const Login = (props) => {
             <Container>
                 <Nav>
                     <a href="/">
-                        <img src="/assets/login-logo.svg" alt="" />
+                        <img src="/assets/login-logo.svg" alt="login image" />
                     </a>
                     <div>
                         <Join>Join now</Join>
                         <SignIn>Sign in</SignIn>
                     </div>
                 </Nav>
+                <Section>
+                    <Hero>
+                        <h1>Welcome to your professional community</h1>
+                        <img src="/assets/login-hero.svg" alt="login-hero" />
+                    </Hero>
+                    <Form>
+                        <Google>
+                            <img src="/assets/google.svg" alt="login with google" />
+                            Sign in with Google
+                        </Google>
+                    </Form>
+                </Section>
             </Container>
         )
 };
@@ -65,7 +77,7 @@ line-height:40px;
 border-radius:24px;
 text-align:center;
 background-color:rgba(0,0,0,0);
-transation-duration:167ms;
+transition-duration:0.3s;
 &:hover{
     background-color:rgba(112,181,249,0.15);
     cursor:pointer;
@@ -76,6 +88,86 @@ transation-duration:167ms;
     padding:10px 10px;
     
 }
+`;
+
+const Section = styled.section`
+    display:flex;
+    align-content:start;
+    min-height:700px;
+    padding-bottom:138px;
+    padding-top:40px;
+    padding:60px 0;
+    position:relative;
+    flex-wrap:wrap;
+    width:100%;
+    max-width:1128px;
+    align-items:center;
+    margin:auto;
+    @media(max-width:768px){
+        margin:auto;
+        min-height:0px;
+    }
+`;
+
+const Hero = styled.div`
+    width:100%;
+    h1{
+        padding-bottom:0;
+        width:55%;
+        font-size:56px;
+        color:#2977c9;
+        font-weight:200;
+        line-height:70px;
+        @media(max-width:768px)
+        {
+            text-align:center;
+            font-size:20px;
+            width:100%;
+            line-height:2;
+        }
+    }
+    img{
+        // z-index:-1;
+        width:700px;
+        height:670px;
+        position:absolute;
+        bottom:-2px;
+        right:-150px;
+        @media(max-width:768px){
+            top:230px;
+            width:initial;
+            position:initial;
+            height:initial; 
+        }
+    }
+`;
+
+const Form = styled.div`
+    margin-top:100px;
+    width:408px;
+    @media(max-width:768px){
+        margin-top:20px;
+    }
+`;
+const Google = styled.button`
+    display:flex;
+    justify-content:center;
+    background-color:#fff;
+    align-items:center;
+    height:56px;
+    width:100%;
+    border-radius:28px;
+    box-shadow:inset 0 0 0 1px rgb(0 0 0 / 60%), inset 0 0 0 2px rgb(0 0 0 / 0%) inset 0 0 0 1px rgb(0 0 0 /0%);
+    vertical-align:middle;
+    z-index:0;
+    transition-duration:167ms;
+    font-size:20px;
+    color:rgba(0,0,0,0.6);
+    &:hover{
+        background-color:rgba(207,207,207,0.25);
+        cursor:pointer;
+        color:rgba(0,0,0,0.75);
+    }
 `;
 
 export default Login;
