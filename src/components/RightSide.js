935c8a98c1b1437822ee9a76a3220f0fd8d02267
shivamvora <shivampatel27102001@gmail.com>
2021-05-11 23:24:16 +0530

    continue...

---
+++ src/components/RightSide.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+
+const RightSide = (props) => {
+    return(
+        <Container>
+            RightSide
+        </Container>
+    );
+};
+
+const Container = styled.div`
+    grid-area:right;
+    
+`;
+
+export default RightSide;
