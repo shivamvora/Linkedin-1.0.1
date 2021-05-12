d337da9cc488874037b598a0b77ff6cd865fab5d
shivamvora <shivampatel27102001@gmail.com>
2021-05-12 13:11:08 +0530

    main page

--- src/components/LeftSide.js
+++ src/components/LeftSide.js
@@ -14,8 +14,41 @@ const LeftSide = (props) => {
                         <AddPhotoText>Add a photo</AddPhotoText>
                     </a>
                 </UserInfo>
-                
+                <Widget>
+                    <a>
+                        <div>
+                            <span>Connections</span>
+                            <span>Grow your network</span>
+                        </div>
+                        <img src="/assets/widget-icon.svg" />
+                    </a>
+                </Widget>
+                <Item>
+                    <span>
+                        <img src="/assets/item-icon.svg"/>
+                        My Items
+                    </span>
+                </Item>
             </ArtCard>
+
+            <CommunityCard>
+                <a>
+                    <span>Groups</span>
+                </a>    
+                <a>
+                    <span>
+                        Events
+                        <img src="/assets/plus-icon.svg" />
+                    </span>
+                </a>
+                <a>
+                    <span>Follow Hashtags</span>
+                </a>
+                <a>
+                    <span>Discover more</span>
+                </a>
+            </CommunityCard>
+
         </Container>
     );
 };
@@ -83,6 +116,92 @@ const AddPhotoText = styled.div`
     font-size:12px;
     line-height:1.33;
     font-weight:400;
+`;
+
+const Widget = styled.div`
+    border-bottom: 1px solid rgba(0,0,0,0.15);
+    padding-top: 12px;
+    padding-bottom: 12px;
+    
+    & > a {
+       text-decoration: none;
+       display:flex;
+       justify-content: space-between;
+       cursor:pointer;
+       align-items:center;
+       padding:4px 12px;
+    }
+    &:hover{
+        background-color:rgba(0,0,0,0.08);
+    }
+    div{
+        display:flex;
+        flex-direction:column;
+        text-align:left;
+        span{
+            font-size:12px;
+            line-height:1.333;
+            &:first-child{
+                color:rgba(0,0,0,0.6);
+            }
+            &:nth-child(2){
+                color:rgba(0,0,0.1);
+            }
+        }
+    }
+
+    svg {
+        color:rgba(0,0,0.1);
+    }
+`;
+
+const Item = styled.a`
+    border-color:rgba(0,0,0,0.8);
+    text-align:left;
+    padding:12px;
+    font-size:12px;
+    display:block;
+    span {
+        display:flex;
+        align-items:center;
+        color:rgba(0,0,0.1);
+        svg{
+            color:rgba(0,0,0,0.6);
+        }
+    }
+    &:hover{
+        background-color:rgba(0,0,0,0.08);
+    }
+`;
+
+const CommunityCard = styled(ArtCard)`
+    padding:8px 0 0;
+    text-align:left;
+    display:flex;
+    flex-direction:column;
+    a {
+        color:black;
+        padding:4px 12px 4px 12px;
+        font-size:12px;
+        &:hover{
+            color:#0a66c2;
+            cursor:pointer;        
+        }
+        span{
+            display:flex;
+            align-items:center;
+            justify-content:space-between;
+        }
+        &:last-child{
+            color: rgba(0,0,0,0.6);
+            text-decoration:none;
+            border-top:1px solid #d6cec2;
+            padding:12px;
+            &:hover{
+                background-color:rgba(0,0,0,0.09);
+            }
+        }
+    }
 `;
 
 export default LeftSide;
